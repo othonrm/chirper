68ba5a7ab3aa2f57ee6428d6108cd11f154c809c
Jose Othon <othon200@gmail.com>
2026-03-05 12:55:56 -0300

    Edit layout + wire up edit, update, and destroy (delete) routes and controller logic.

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ChirpController extends Controller
 {
@@ -33,38 +34,61 @@ class ChirpController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'message' => [
+                'required',
+                'string',
+                'max:255',
+                // Rule::unique('chirps')->where(function ($query) use ($user) {
+                //     return $query->where('user_id', $user->id);
+                // }),
+            ],
+        ], [
+            'message.required' => 'Write the f*** message!',
+            'message.max' => 'WTF?! This is chirp, not a bible. Save some words!',
+        ]);
+
+        Chirp::create([
+            'message' => $validated['message'],
+        ]);
+
+        return redirect('/')->with('success', 'Chirp created successfully!');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
-    {
-        //
-    }
+    public function show(string $id) {}
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Chirp $chirp)
     {
-        //
+        return view('chirps.edit', compact('chirp'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Chirp $chirp)
     {
-        //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $chirp->update($validated);
+
+        return redirect('/')->with('success', 'Chirp successfully updated!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Chirp $chirp)
     {
-        //
+        $chirp->delete();
+
+        return redirect('/')->with('success', 'Successfully deleted your chirp!');
     }
 }
